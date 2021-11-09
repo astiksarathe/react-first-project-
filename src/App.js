f5545517fbbd3a5705dc0cb109144c2c82f14117
astiksarathe <92151027+astiksarathe@users.noreply.github.com>
2021-11-09 21:13:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 //import Deployment from "./deployment/Deployment";
 import "react-table-6/react-table.css";
 import TimeTracking from "./time-tracking/TimeTracking";
-
+import Overview from "./overview/Overview";
 const App = () => {
   return (
     <div className="App">
       //<Deployment />
-    <TimeTracking />
+//     <TimeTracking />
+     <Overview />
     </div>
   );
 };
